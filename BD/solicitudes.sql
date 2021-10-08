a6d041a12b69fda807022fcd5fa3ff55f7805f0d
Richard Fonseca <rfonseca2110@gmail.com>
2021-10-08 17:17:29 -0500

    DB

--- BD/solicitudes.sql
+++ BD/solicitudes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 05-10-2021 a las 16:16:27
--- Versión del servidor: 10.3.31-MariaDB-0ubuntu0.20.04.1
--- Versión de PHP: 7.4.16
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-10-2021 a las 00:15:25
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `rycenergy_solicitudes`
+-- Base de datos: `solicitudes`
 --
 
 -- --------------------------------------------------------
@@ -123,6 +123,19 @@ INSERT INTO `sc_estados` (`est_id_i`, `est_nombre_v`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `sc_evidencia`
+--
+
+CREATE TABLE `sc_evidencia` (
+  `id_evidencia_i` int(11) NOT NULL,
+  `id_asig_i` int(11) NOT NULL,
+  `ruta_evidencia` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_evidencia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `sc_horas`
 --
 
@@ -208,7 +221,7 @@ CREATE TABLE `sc_perfiles` (
 INSERT INTO `sc_perfiles` (`perf_id_i`, `perf_nombre_v`, `perf_asig_i`, `perf_add_i`, `perf_upd_i`, `perf_del_i`) VALUES
 (1, 'SUPERADMINISTRADOR', 1, 0, 0, 0),
 (2, 'ADMINISTRADOR', 1, 1, 1, 1),
-(3, 'USUARIO', 0, 0, 0, 0),
+(3, 'CLIENTE', 0, 0, 0, 0),
 (4, 'TECNICO', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
@@ -251,22 +264,25 @@ CREATE TABLE `sc_solicitudes` (
   `sol_fecha_cita_d` date DEFAULT NULL,
   `sol_usu_id_i` int(11) DEFAULT NULL,
   `sol_ban_id_i` int(11) DEFAULT NULL,
-  `sol_fecha_solucion` date DEFAULT NULL
+  `sol_fecha_solucion` date DEFAULT NULL,
+  `sol_ruta_ot_v` varchar(255) NOT NULL,
+  `sol_id_tps_i` int(11) NOT NULL,
+  `sol_aplica_i` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `sc_solicitudes`
 --
 
-INSERT INTO `sc_solicitudes` (`sol_id_i`, `sol_suc_id_i`, `sol_tec_usu_id_i`, `sol_fecha_solicitud_d`, `sol_hora_cita_v`, `sol_requerimiento_t`, `sol_orden_trabajo`, `sol_observaciones_t`, `sol_prio_id`, `sol_est_id_i`, `sol_fecha_cita_d`, `sol_usu_id_i`, `sol_ban_id_i`, `sol_fecha_solucion`) VALUES
-(2, 3, 0, '2021-09-15', NULL, 'Ayuda!!! Ayuda!!!', '202109152', 'ya esta arreglado el tema!', 2, 5, NULL, 1, 1, '2021-10-05'),
-(4, 3, 0, '2021-09-15', NULL, 'Ayuda!!! Ayuda!!!', '202109155', 'Listo quedo solucionado!', 1, 5, NULL, 1, 1, '2021-10-05'),
-(5, 3, 0, '2021-09-15', NULL, 'Ayuda!!!', '202109156', 'solucionado', 3, 5, NULL, 1, 1, NULL),
-(6, 3, 0, '2021-09-15', NULL, 'Ayuda!!!', '202109157', 'Prueba de solucion!', 2, 5, NULL, 1, 1, NULL),
-(9, 3, 0, '2021-09-15', NULL, 'Hola!!', '2021091510', 'Solucionado!!!', 3, 5, NULL, 1, 1, NULL),
-(13, 3, 0, '2021-09-23', NULL, 'urgente', '202109233', 'Vamos a cerrarlo, para ver que sucede', 2, 5, NULL, 5, 1, NULL),
-(15, 2, 2, '2021-10-05', NULL, 'Vamos a modificar todo el cableado estructural del sitio!', '202110052', 'vamos a ver que sucede!', 1, 4, NULL, 5, 1, NULL),
-(16, 3, 2, '2021-10-05', NULL, 'Pruebas nbuevam,entye!', '202110053', 'observen por favor!', 1, 4, NULL, 5, 1, NULL);
+INSERT INTO `sc_solicitudes` (`sol_id_i`, `sol_suc_id_i`, `sol_tec_usu_id_i`, `sol_fecha_solicitud_d`, `sol_hora_cita_v`, `sol_requerimiento_t`, `sol_orden_trabajo`, `sol_observaciones_t`, `sol_prio_id`, `sol_est_id_i`, `sol_fecha_cita_d`, `sol_usu_id_i`, `sol_ban_id_i`, `sol_fecha_solucion`, `sol_ruta_ot_v`, `sol_id_tps_i`, `sol_aplica_i`) VALUES
+(2, 3, 0, '2021-09-15', NULL, 'Ayuda!!! Ayuda!!!', '202109152', 'ya esta arreglado el tema!', 2, 5, NULL, 1, 1, '2021-10-05', '', 0, 0),
+(4, 3, 0, '2021-09-15', NULL, 'Ayuda!!! Ayuda!!!', '202109155', 'Listo quedo solucionado!', 1, 5, NULL, 1, 1, '2021-10-05', '', 0, 0),
+(5, 3, 0, '2021-09-15', NULL, 'Ayuda!!!', '202109156', 'solucionado', 3, 5, NULL, 1, 1, NULL, '', 0, 0),
+(6, 3, 0, '2021-09-15', NULL, 'Ayuda!!!', '202109157', 'Prueba de solucion!', 2, 5, NULL, 1, 1, NULL, '', 0, 0),
+(9, 3, 0, '2021-09-15', NULL, 'Hola!!', '2021091510', 'Solucionado!!!', 3, 5, NULL, 1, 1, NULL, '', 0, 0),
+(13, 3, 0, '2021-09-23', NULL, 'urgente', '202109233', 'Vamos a cerrarlo, para ver que sucede', 2, 5, NULL, 5, 1, NULL, '', 0, 0),
+(15, 2, 2, '2021-10-05', NULL, 'Vamos a modificar todo el cableado estructural del sitio!', '202110052', 'vamos a ver que sucede!', 1, 4, NULL, 5, 1, NULL, '', 0, 0),
+(16, 3, 2, '2021-10-05', NULL, 'Pruebas nbuevam,entye!', '202110053', 'observen por favor!', 1, 4, NULL, 5, 1, NULL, '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -316,6 +332,17 @@ INSERT INTO `sc_tipo_documento` (`tipd_id_i`, `tipd_descripcion_v`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `sc_tipo_servicio`
+--
+
+CREATE TABLE `sc_tipo_servicio` (
+  `id_tps_i` int(11) DEFAULT NULL,
+  `desc_tps_v` varchar(255) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `sc_usuarios`
 --
 
@@ -330,17 +357,30 @@ CREATE TABLE `sc_usuarios` (
   `usu_banco_i` int(11) NOT NULL,
   `usu_fecha_registro_d` date NOT NULL,
   `usu_usuario_v` varchar(255) NOT NULL,
-  `usu_password_v` varchar(255) NOT NULL
+  `usu_password_v` varchar(255) NOT NULL,
+  `usu_correo_v` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `sc_usuarios`
 --
 
-INSERT INTO `sc_usuarios` (`usu_id_i`, `usu_tip_doc_id_i`, `usu_documento_v`, `usu_nombre_v`, `usu_apellido_v`, `usu_per_id_i`, `usu_est_id_i`, `usu_banco_i`, `usu_fecha_registro_d`, `usu_usuario_v`, `usu_password_v`) VALUES
-(2, 1, '1143231494', 'Jose David X', 'Giron Martinez', 4, 1, 1, '2021-09-20', 'jgiron', 'e10adc3949ba59abbe56e057f20f883e'),
-(3, 2, '90012850602', 'Juan Sebastian', 'Veron', 3, 0, 1, '2021-09-20', 'jveron', 'e10adc3949ba59abbe56e057f20f883e'),
-(5, 4, '7789654-8', 'ID345 Solutions', 'SAS', 2, 0, 1, '2021-09-20', 'id345', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `sc_usuarios` (`usu_id_i`, `usu_tip_doc_id_i`, `usu_documento_v`, `usu_nombre_v`, `usu_apellido_v`, `usu_per_id_i`, `usu_est_id_i`, `usu_banco_i`, `usu_fecha_registro_d`, `usu_usuario_v`, `usu_password_v`, `usu_correo_v`) VALUES
+(2, 1, '1143231494', 'Jose David X', 'Giron Martinez', 4, 1, 1, '2021-09-20', 'jgiron', 'e10adc3949ba59abbe56e057f20f883e', ''),
+(3, 2, '90012850602', 'Juan Sebastian', 'Veron', 3, 0, 2, '2021-10-06', 'jveron', 'e10adc3949ba59abbe56e057f20f883e', ''),
+(5, 4, '7789654-8', 'ID345 Solutions', 'SAS', 2, 0, 1, '2021-09-20', 'id345', 'e10adc3949ba59abbe56e057f20f883e', ''),
+(6, 2, '988989', 'sadio', 'cetre', 4, 1, 1, '2021-10-06', 'sadio10', 'fcea920f7412b5da7be0cf42b8c93759', 'sadio10@meet.com'),
+(7, 1, '79.898.909', 'JUAN CARLOS', 'BARRETO DIAZ', 4, 1, 1, '2021-10-06', 'JBARRETO', 'bfda238a0eb04e16f50dbb43ff7ad391', ''),
+(8, 1, '1.064.977.677', 'DAIRO DAVID', 'MARTELO YANEZ', 4, 1, 1, '2021-10-06', 'DMARTELO', '88a22850d8a2e3277839786fe5187f35', ''),
+(9, 1, '1.070.750.699', 'EDWUIN ALBEIRO', 'RODRIGUEZ RODRIGUEZ', 4, 1, 1, '2021-10-06', 'ERODRIGUEZ', '9d6191901a49e4b1ab987c58f59ae200', ''),
+(10, 1, '11.245.779', 'HUGO HERNAN ', 'MORA', 4, 1, 1, '2021-10-06', 'HMORA', '9f977baf63ab836a92edb7a08a3c6287', ''),
+(11, 1, '1.030.580.088', 'JEFFERSON', 'CASTAÑEDA DUQUE', 4, 1, 1, '2021-10-06', 'JCASTAÑEDA', '8aa804372f2d514b0b3897ce38f13ba3', ''),
+(12, 1, '93123660', 'JOSE IVAN', 'CASTAÑEDA GARCIA', 4, 1, 1, '2021-10-06', 'JCASTAÑEDAG', '6eaaa7f448f2dec8a985b0928765c532', ''),
+(13, 1, ' 1070754093', 'MICHEL STIVEN', 'RODRIGUEZ RINCON', 4, 1, 1, '2021-10-06', 'MRODRIGUEZ', 'aa321f5c1b976451a6cb79a771fa85c1', ''),
+(14, 1, '3155716', 'RAUL ARNULFO', 'RODRIGUEZ GONZALEZ', 4, 1, 1, '2021-10-06', 'RRODRIGUEZ', 'bd7607cc2bd7f27d3dcb8272392de66b', ''),
+(15, 1, '18877506', 'WILMER EDUARDO', 'GARCIA JARABA', 1, 1, 1, '2021-10-06', 'WGARCIA ', '96028d75203a92231ad26ac34d8f3094', ''),
+(16, 1, '1075312638', 'ANDRES FELIPE', 'VANEGAS CORTES', 1, 1, 1, '2021-10-06', 'AVANEGAS ', '58486c956323afd7deee089011c8b23d', ''),
+(17, 1, '1033775734', 'JEISSON', 'RODRIGUEZ JIMENEZ', 4, 1, 1, '2021-10-06', 'JRODRIGUEZ ', '1e33e212d8f0305ec82d6317adc11685', '');
 
 --
 -- Índices para tablas volcadas
@@ -369,6 +409,12 @@ ALTER TABLE `sc_bancos`
 --
 ALTER TABLE `sc_estados`
   ADD PRIMARY KEY (`est_id_i`);
+
+--
+-- Indices de la tabla `sc_evidencia`
+--
+ALTER TABLE `sc_evidencia`
+  ADD PRIMARY KEY (`id_evidencia_i`);
 
 --
 -- Indices de la tabla `sc_horas`
@@ -447,6 +493,12 @@ ALTER TABLE `sc_estados`
   MODIFY `est_id_i` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT de la tabla `sc_evidencia`
+--
+ALTER TABLE `sc_evidencia`
+  MODIFY `id_evidencia_i` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `sc_horas`
 --
 ALTER TABLE `sc_horas`
@@ -492,7 +544,7 @@ ALTER TABLE `sc_tipo_documento`
 -- AUTO_INCREMENT de la tabla `sc_usuarios`
 --
 ALTER TABLE `sc_usuarios`
-  MODIFY `usu_id_i` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `usu_id_i` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
